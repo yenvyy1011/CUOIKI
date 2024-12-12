7c3e33d1f615989f8b2b705b43e1f792729456f7
yenvy <ntyvizz1011@gmail.com>
2024-12-13 00:17:05 +0700

    thêm file

---
+++ vd1.php
@@ -0,0 +1,22 @@
+<pre><?php
+//nếu n = x thì tồn tại ko thì bằng rỗng
+$n=isset($_GET['x'])?$_GET['x']:'';
+$m = $_GET['y']??0;  echo "n=$n <br> m=$m, <hr>";
+//nếu get có thì request có
+print_r($_GET);
+print_r($_POST);
+print_r($_REQUEST);
+print_r($_FILES);
+$n=$_POST['x']??'';
+
+?></pre>
+<h1>Chao <?php echo $n ?> </h1>
+<?php 
+if($_FILES['f']['error']==0)
+{
+    $file =$_FILES['f'];
+    $tenfile =$file['name'];
+    $filetam =$file['tmp_name'];
+    move_uploaded_file($filetam,"img/$tenfile");
+    echo "<img src='img/$tenfile' width=400> ";
+}
---
+++ vd4.php
@@ -0,0 +1,6 @@
+<form action="vd4.php">
+    Ten: <input type="text" name="t" id="">
+    <input type="submmit" value="Tim kiem">
+</form>
+<?php
+ ?>
