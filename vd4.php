580ab6ca14cd19eefee212cba43b62f179f612d9
yenvy <ntyvizz1011@gmail.com>
2024-12-13 12:20:29 +0700

    aaa

--- vd4.php
+++ vd4.php
@@ -1,6 +1,8 @@
 <form action="vd4.php">
     Ten: <input type="text" name="t" id="">
     <input type="submmit" value="Tim kiem">
+    aaa: <input type="text" name="t" id="">
+    <input type="submmit" value="Tim kiem">
 </form>
 <?php
  ?>
