d949062ffebafe44dbfe4f2885d5f7b14a719023
yenvy <ntyvizz1011@gmail.com>
2024-12-14 13:23:02 +0700

    suafile

--- vd4.php
+++ vd4.php
@@ -1,6 +1,8 @@
 <form action="vd4.php">
     Ten: <input type="text" name="t" id="">
     <input type="submmit" value="Tim kiem">
+    Ten: <input type="text" name="t" id="">
+    <input type="submmit" value="Tim kiem">
 </form>
 <?php
  ?>
